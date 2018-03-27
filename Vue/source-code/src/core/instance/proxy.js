6ae0f7212482c11655337ac149bfb125a003b9ca
yuyang <421736079@qq.com>
2018-03-28 02:09:46 +0800

    Comment: add Vue core/instance/proxy

--- Vue/source-code/src/core/instance/proxy.js
+++ Vue/source-code/src/core/instance/proxy.js
@@ -6,6 +6,8 @@ import { warn, makeMap, isNative } from '../util/index'
 let initProxy
 
 if (process.env.NODE_ENV !== 'production') {
+  // 全局中允许出现的变量或方法(还有一些基本型)
+  // 主要用于编译模板时, 判断模板中是否有引用未定义的实例属性(prop, data, computed, methods, inject...)
   const allowedGlobals = makeMap(
     'Infinity,undefined,NaN,isFinite,isNaN,' +
     'parseFloat,parseInt,decodeURI,decodeURIComponent,encodeURI,encodeURIComponent,' +
@@ -27,6 +29,9 @@ if (process.env.NODE_ENV !== 'production') {
   const hasProxy =
     typeof Proxy !== 'undefined' && isNative(Proxy)
 
+  // 兼容proxy的时候, 限制config.keyCodes的写入,
+  // 不能重写内置的修饰符, 此为渐进增强, 当环境
+  // 中不支持Proxy时候, 这一步直接忽略
   if (hasProxy) {
     const isBuiltInModifier = makeMap('stop,prevent,self,ctrl,shift,alt,meta,exact')
     config.keyCodes = new Proxy(config.keyCodes, {
@@ -46,9 +51,11 @@ if (process.env.NODE_ENV !== 'production') {
     has (target, key) {
       const has = key in target
       const isAllowed = allowedGlobals(key) || key.charAt(0) === '_'
+      // 存在于target上, 且不是内置可以的全局属性或方法, 则警告
       if (!has && !isAllowed) {
         warnNonPresent(target, key)
       }
+      // !has && !isAllowed 这种情况也返回true, 虽然实例上没有...
       return has || !isAllowed
     }
   }
@@ -65,10 +72,12 @@ if (process.env.NODE_ENV !== 'production') {
   initProxy = function initProxy (vm) {
     if (hasProxy) {
       // determine which proxy handler to use
+      // 决定是否使用Proxy
       const options = vm.$options
       const handlers = options.render && options.render._withStripped
         ? getHandler
         : hasHandler
+      // 通过__renderProxy来代理vm上的属性
       vm._renderProxy = new Proxy(vm, handlers)
     } else {
       vm._renderProxy = vm
