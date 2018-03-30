5ae47d2bf5cd95309738e7a4d59c63999f748fb2
yuyang <421736079@qq.com>
2018-03-30 23:03:02 +0800

    Comment: add Vue core/instance/render-helpers/resolve-filter

--- Vue/source-code/src/core/instance/render-helpers/resolve-filter.js
+++ Vue/source-code/src/core/instance/render-helpers/resolve-filter.js
@@ -5,6 +5,10 @@ import { identity, resolveAsset } from 'core/util/index'
 /**
  * Runtime helper for resolving filters
  */
+// 用于解析filters的运行时辅助函数
 export function resolveFilter (id: string): Function {
+  // 如果存在filter则照常返回， 不存在就返回identity函数(此函数返回其第一个参数自身)
+  // 即如果我们使用filter时, 用到的filter是不存在的, 则返回自身
+  // TODO: 这里需要加一句判断吧, 判断环境
   return resolveAsset(this.$options, 'filters', id, true) || identity
 }
