0ddbacf3ddb62a793c97c2379c522932022cff66
yuyang <421736079@qq.com>
2018-04-06 01:11:44 +0800

    Comment: add Vue core/vdom/helpers/is-async-placeholder

--- main/Vue/source-code/src/core/vdom/helpers/is-async-placeholder.js
+++ main/Vue/source-code/src/core/vdom/helpers/is-async-placeholder.js
@@ -1,5 +1,6 @@
 /* @flow */
-
+// 判断是否是异步组件占位符
+// 判断依据是vnode的isComment和asyncFactory都为真 
 export function isAsyncPlaceholder (node: VNode): boolean {
   return node.isComment && node.asyncFactory
 }
