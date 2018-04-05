3bee83364521e04928028883013fd7a942ccc08c
yuyang <421736079@qq.com>
2018-04-06 01:08:11 +0800

    Comment: add Vue core/vdom/helpers/get-first-component-child

--- main/Vue/source-code/src/core/vdom/helpers/get-first-component-child.js
+++ main/Vue/source-code/src/core/vdom/helpers/get-first-component-child.js
@@ -3,10 +3,12 @@
 import { isDef } from 'shared/util'
 import { isAsyncPlaceholder } from './is-async-placeholder'
 
+// 获取第一个子组件
 export function getFirstComponentChild (children: ?Array<VNode>): ?VNode {
   if (Array.isArray(children)) {
     for (let i = 0; i < children.length; i++) {
       const c = children[i]
+      // 已定义(即不为null或undefined), 或为异步组件占位符
       if (isDef(c) && (isDef(c.componentOptions) || isAsyncPlaceholder(c))) {
         return c
       }
