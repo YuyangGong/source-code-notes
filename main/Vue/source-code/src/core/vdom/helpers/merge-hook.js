a6dadeecc00296f9ac6c6363bf809246675a401d
yuyang <421736079@qq.com>
2018-04-06 02:09:49 +0800

    Comment: add Vue core/vdom/helpers/merge-hook

--- main/Vue/source-code/src/core/vdom/helpers/merge-hook.js
+++ main/Vue/source-code/src/core/vdom/helpers/merge-hook.js
@@ -15,20 +15,28 @@ export function mergeVNodeHook (def: Object, hookKey: string, hook: Function) {
     hook.apply(this, arguments)
     // important: remove merged hook to ensure it's called only once
     // and prevent memory leak
+    // 移除已经merge的钩子去确保其只调用一次并且避免内存泄露
+    // WHY 这里为什么要确保只调用一次? 钩子不是每次都有用么？
     remove(invoker.fns, wrappedHook)
   }
 
   if (isUndef(oldHook)) {
     // no existing hook
+    // 当原本不存在钩子时
+    // 这里用数组包一层是必须的, 需要包装invoker.fns为数组,
+    // 后续需要对这个数组进行push和remove操作
     invoker = createFnInvoker([wrappedHook])
   } else {
     /* istanbul ignore if */
     if (isDef(oldHook.fns) && isTrue(oldHook.merged)) {
       // already a merged invoker
+      // 当oldHook已经merge过时, 已经把包装后的新钩子push进调用器的fns数组中即可
       invoker = oldHook
       invoker.fns.push(wrappedHook)
     } else {
       // existing plain hook
+      // 当存在oldHook且其没有merge过时,
+      // 以oldHook和wrappedHook创建数组并生成调用器(Invoker)
       invoker = createFnInvoker([oldHook, wrappedHook])
     }
   }
