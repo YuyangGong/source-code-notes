df5b9b4e57bf3481a8c83f10300407e0d47acaf5
yuyang <421736079@qq.com>
2018-04-16 01:18:16 +0800

    Comment: add Vue core/vdom/create-element

--- main/Vue/source-code/src/core/vdom/create-element.js
+++ main/Vue/source-code/src/core/vdom/create-element.js
@@ -25,6 +25,7 @@ const ALWAYS_NORMALIZE = 2
 
 // wrapper function for providing a more flexible interface
 // without getting yelled at by flow
+// 提供弹性接口的包装函数, 用于避免flow报错
 export function createElement (
   context: Component,
   tag: any,
@@ -51,6 +52,8 @@ export function _createElement (
   children?: any,
   normalizationType?: number
 ): VNode | Array<VNode> {
+  // 不能使用已经observe的data作为VNode的数据, 
+  // 如果出现这种情况则直接返回空的VNode
   if (isDef(data) && isDef((data: any).__ob__)) {
     process.env.NODE_ENV !== 'production' && warn(
       `Avoid using observed data object as vnode data: ${JSON.stringify(data)}\n` +
@@ -60,14 +63,18 @@ export function _createElement (
     return createEmptyVNode()
   }
   // object syntax in v-bind
+  // v-bind中的object语法
+  // 在<component :is="xxx"></component>中, is对应的是组件
   if (isDef(data) && isDef(data.is)) {
     tag = data.is
   }
   if (!tag) {
     // in case of component :is set to falsy value
+    // 当is被设定为假值时候, 返回空VNode
     return createEmptyVNode()
   }
   // warn against non-primitive key
+  // 避免使用非基本值作为key
   if (process.env.NODE_ENV !== 'production' &&
     isDef(data) && isDef(data.key) && !isPrimitive(data.key)
   ) {
@@ -80,6 +87,8 @@ export function _createElement (
     }
   }
   // support single function children as default scoped slot
+  // 如果children的第一个元素为函数时候, 使其作为
+  // 默认的scopedSlots
   if (Array.isArray(children) &&
     typeof children[0] === 'function'
   ) {
@@ -98,12 +107,15 @@ export function _createElement (
     ns = (context.$vnode && context.$vnode.ns) || config.getTagNamespace(tag)
     if (config.isReservedTag(tag)) {
       // platform built-in elements
+      // 如果是保留的tag名, 则针对不同平台, 
+      // 对tag进行解析(一般是直接返回源tag字符串)
       vnode = new VNode(
         config.parsePlatformTagName(tag), data, children,
         undefined, undefined, context
       )
     } else if (isDef(Ctor = resolveAsset(context.$options, 'components', tag))) {
       // component
+      // 创建组件
       vnode = createComponent(Ctor, data, context, children, tag)
     } else {
       // unknown or unlisted namespaced elements
@@ -116,6 +128,7 @@ export function _createElement (
     }
   } else {
     // direct component options / constructor
+    // 当tag直接为component选项或者构造函数时
     vnode = createComponent(tag, data, context, children)
   }
   if (Array.isArray(vnode)) {
@@ -125,21 +138,27 @@ export function _createElement (
     if (isDef(data)) registerDeepBindings(data)
     return vnode
   } else {
+    // vnode为定义时, 创建空的VNode
     return createEmptyVNode()
   }
 }
 
+// 绑定vnode及其children的ns
 function applyNS (vnode, ns, force) {
   vnode.ns = ns
   if (vnode.tag === 'foreignObject') {
     // use default namespace inside foreignObject
+    // 对于foreignObject, 使用默认的命名空间
+    // WHY: foreignObject是什么类型的tag?
     ns = undefined
     force = true
   }
+  // 递归对组件applyNS
   if (isDef(vnode.children)) {
     for (let i = 0, l = vnode.children.length; i < l; i++) {
       const child = vnode.children[i]
       if (isDef(child.tag) && (
+        // child.ns存在, 或者 force为真且child.tag不为svg时候, 才绑定子元素的NS
         isUndef(child.ns) || (isTrue(force) && child.tag !== 'svg'))) {
         applyNS(child, ns, force)
       }
@@ -150,6 +169,7 @@ function applyNS (vnode, ns, force) {
 // ref #5318
 // necessary to ensure parent re-render when deep bindings like :style and
 // :class are used on slot nodes
+// 当在slot节点上deep绑定:style和:class, 必需确保父实例重新渲染
 function registerDeepBindings (data) {
   if (isObject(data.style)) {
     traverse(data.style)
