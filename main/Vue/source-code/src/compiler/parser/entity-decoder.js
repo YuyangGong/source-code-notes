5a508d35007715d5cc6644f799235bf2d4f36993
yuyang <421736079@qq.com>
2018-04-25 18:51:14 +0800

    Comment: add Vue src/compiler/parser/entity-decoder

--- main/Vue/source-code/src/compiler/parser/entity-decoder.js
+++ main/Vue/source-code/src/compiler/parser/entity-decoder.js
@@ -3,6 +3,7 @@
 let decoder
 
 export default {
+  // html entity解码, 这里采用的是借助dom的方式
   decode (html: string): string {
     decoder = decoder || document.createElement('div')
     decoder.innerHTML = html
