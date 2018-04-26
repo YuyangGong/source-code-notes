e34c974d733ad9655dde6585ae8beb39effb8c21
yuyang <421736079@qq.com>
2018-04-26 22:42:32 +0800

    Comment: add Vue src/compiler/directives/on

--- main/Vue/source-code/src/compiler/directives/on.js
+++ main/Vue/source-code/src/compiler/directives/on.js
@@ -2,6 +2,8 @@
 
 import { warn } from 'core/util/index'
 
+// v-on如果是用于对象, 如<button v-on="{ mousedown: doThis, mouseup: doThat }"></button>,
+// 则不支持modifiers
 export default function on (el: ASTElement, dir: ASTDirective) {
   if (process.env.NODE_ENV !== 'production' && dir.modifiers) {
     warn(`v-on without argument does not support modifiers.`)
